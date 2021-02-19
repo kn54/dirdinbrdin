2f8cddc943f4d13200bb251f42b69e7d2b1f0845
kaia neuman <daniellneuman@gmail.com>
2021-02-19 21:44:28 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// lkaalkjsfljweajfwkejfKKKKKKKKKKKKKKKKKKKKKKKKIAIA YA
+let ptag = document.getElementById('ptag') ;
+
+let dog = document.getElementById('dog');
+
+
+
+dog.onclick=function() {
+  if (ptag.style.display === "none") {
+    ptag.style.display = 'block';
+  } else {
+    ptag.style.display = 'none';
+  }
+}
+
+
+let cat = {};
+
+cat.name = "pinky";
+cat.weight = 10;
+species = "cat";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
